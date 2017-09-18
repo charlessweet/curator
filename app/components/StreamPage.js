1e6da46786e0c5f01c2c4a57faa1fb17091c81ac
Chuck Sweet <chuck.sweet@gmail.com>
2017-09-17 21:18:07 -0700

    Trial and error to isolate why webpack can't find StreamInfo.

--- app/components/StreamPage.js
+++ app/components/StreamPage.js
@@ -8,7 +8,7 @@ import ArticleCardList from '../controls/ArticleCardList'
 import ArticlePost from '../controls/Articles/ArticlePost'
 import {loadStreamAsync, reviewArticle} from '../actions/index'
 import StoreObserver from '../services/StoreObserver'
-import StreamInfo from '../controls/Stream/Streaminfo.js'
+import StreamInfo from '../controls/Stream/Streaminfo'
 
 class StreamPageUnwrapped extends React.Component{
 	constructor(props){
