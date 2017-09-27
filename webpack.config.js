b0ce7de72258f6d3d2c7848e73ef3817dd2ad8d2
Chuck Sweet <chuck.sweet@gmail.com>
2017-09-26 21:34:01 -0700

    Second try.

--- webpack.config.js
+++ webpack.config.js
@@ -51,7 +51,7 @@ module.exports = {
 		path:__dirname + '/build'
 	},
 	plugins: [HTMLWebpackPluginConfig,
-        new webpack.DefinePlugin({
+        new DefinePlugin({
             BIAS_SERVER_URL: (process.env.BIAS_SERVER_URL || "http://localhost:3000"),
             FB_APP_ID: (process.env.FB_APP_ID || "382449245425765"),
             BC_APP_ID: (process.env.BC_APP_ID || "0909367047e24c43956ae4511cb28f00"),
