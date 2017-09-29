b1c9490d395dd6d46ce5544f1bc3e594c64dc34e
Chuck Sweet <chuck.sweet@gmail.com>
2017-09-29 15:23:01 -0700

    Updated the settings.

--- app/model/Settings.js
+++ app/model/Settings.js
@@ -1,7 +1,7 @@
 class Settings{
 	constructor(){
 		this.biasServiceUrl = (BIAS_SERVER_URL || "http://localhost:3000");
-		this.fbAppId = (FB_APP_ID || "382449245425765");
+		this.fbAppId = (FB_APP_ID || "250670435348050");
 		this.biasCheckerAppId = (BC_APP_ID || "0909367047e24c43956ae4511cb28f00");
 		this.biasCheckerSecret = (BC_APP_SECRET || "0e4f843becb044e496a317f3befc5105");
 	}
